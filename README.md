bb90813274f928958fc172cf349054dcc5648550
vassdeniss <72888249+vassdeniss@users.noreply.github.com>
2021-11-14 19:49:04 +0200

    Added gitignore / Implemented basic ui

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# tetris-game-cs-console-app
-The worldwide popular tetris game created in C# as a console app.
+# Tetris in C# Console
+
+The all so popular block stacking game re-created in the console using C# and the .NET Core.
+
+## Visuals
+
+## Created Using
+* Visual Studio 2022
+* .NET Core 3.1
+* C# 8.0
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to star this repository to show your support <3
---
+++ TetrisCsConsole/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace TetrisCsConsole
+{
+    internal class Program
+    {
+        // Settings
+        static int GameFieldHeight = 20;
+        static int GameFieldWidth = 10;
+        static int InfoFieldWidth = 10;
+        static int ConsoleHeight = 1 + GameFieldHeight + 1;
+        static int ConsoleWidth = 1 + GameFieldWidth + 1 + InfoFieldWidth + 1;
+
+        // States 
+        static int Score = 0;
+
+        static void Main(string[] args)
+        {
+            SetConsoleSettings("Tetris in the Console", ConsoleWidth, ConsoleHeight, false, ConsoleColor.Green);
+            DrawBorder();
+            DrawInfo();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Escape)
+                        Environment.Exit(0);
+                }
+
+                Score++;
+                DrawBorder();
+                DrawInfo();
+                Thread.Sleep(35);
+            }
+        }
+
+        static void SetConsoleSettings(string name,
+                                       int consoleWidth,
+                                       int consoleHeight,
+                                       bool isCursorVisible = false,
+                                       ConsoleColor color = ConsoleColor.Green)
+        {
+            Console.Title = name;
+            Console.CursorVisible = isCursorVisible;
+            Console.SetWindowSize(consoleWidth, consoleHeight + 1);
+            Console.SetBufferSize(consoleWidth, consoleHeight + 1);
+            Console.ForegroundColor = color;
+        }
+        
+        static void DrawBorder()
+        {
+            Console.SetCursorPosition(0, 0);
+
+            // Top Line
+            StringBuilder topLine = new StringBuilder();
+            topLine.Append('╔');
+            topLine.Append('═', GameFieldWidth);
+            topLine.Append('╦');
+            topLine.Append('═', InfoFieldWidth);
+            topLine.Append('╗');
+            Console.WriteLine(topLine);
+
+            // Middle Lines
+            for (int i = 0; i < GameFieldHeight; i++)
+            {
+                StringBuilder midLine = new StringBuilder();
+                midLine.Append('║');
+                midLine.Append(new string(' ', GameFieldWidth));
+                midLine.Append('║');
+                midLine.Append(new string(' ', InfoFieldWidth));
+                midLine.Append('║');
+                Console.WriteLine(midLine);
+            }
+
+            // Bottom Line
+            StringBuilder bottomLine = new StringBuilder();
+            bottomLine.Append('╚');
+            bottomLine.Append('═', GameFieldWidth);
+            bottomLine.Append('╩');
+            bottomLine.Append('═', InfoFieldWidth);
+            bottomLine.Append('╝');
+            Console.WriteLine(bottomLine);
+        }
+
+        static void DrawInfo()
+        {
+            Write("Score:", GameFieldWidth + 3, 1);
+            Write(Score.ToString(), GameFieldWidth + 3, 2);
+        }
+
+        static void Write(string text, int col, int row)
+        {
+            Console.SetCursorPosition(col, row);
+            Console.Write(text);
+        }
+    }
+}
