32097746e30e64f1cfe37e8707dfb8d02810a8e9
vassdeniss <72888249+vassdeniss@users.noreply.github.com>
2021-11-15 10:18:54 +0200

    Implemented pieces & pieces visualization & controls

--- TetrisCsConsole/Program.cs
+++ TetrisCsConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 
@@ -7,70 +8,167 @@ namespace TetrisCsConsole
     internal class Program
     {
         // Settings
-        static int GameFieldHeight = 20;
-        static int GameFieldWidth = 10;
-        static int InfoFieldWidth = 10;
-        static int ConsoleHeight = 1 + GameFieldHeight + 1;
-        static int ConsoleWidth = 1 + GameFieldWidth + 1 + InfoFieldWidth + 1;
+        static int GameFieldRow = 20;
+        static int GameFieldCol = 10;
+        static int InfoFieldCol = 10;
+        static int ConsoleHeight = 1 + GameFieldRow + 1;
+        static int ConsoleWidth = 1 + GameFieldCol + 1 + InfoFieldCol + 1;
+        static List<bool[,]> Tetrominos = new List<bool[,]>()
+        {
+            new bool[,] // I
+            {
+                { true, true, true, true }
+            },
+            new bool[,] // O
+            {
+                { true, true },
+                { true, true }
+            },
+            new bool[,] // T
+            {
+                { false, true, false },
+                { true, true, true }
+            },
+            new bool[,] // S
+            {
+                { false, true, true },
+                { true, true, false }
+            },
+            new bool[,] // Z
+            {
+                { true, true, false },
+                { false, true, true }
+            },
+            new bool[,] // J
+            {
+                { true, false, false },
+                { true, true, true }
+            },  
+            new bool[,] // L
+            {
+                { false, false, true },
+                { true, true, true }
+            }
+        };
 
         // States 
         static int Score = 0;
+        static int Frame = 0;
+        static int MoveFrame = 20;
+        static int CurrentTetromino = 4;
+        static int TetrominoRow = 0;
+        static int TetrominoCol = 0;
 
         static void Main(string[] args)
         {
-            SetConsoleSettings("Tetris in the Console", ConsoleWidth, ConsoleHeight, false, ConsoleColor.Green);
+            SetConsoleSettings("Tetris in the Console", ConsoleWidth, ConsoleHeight, false);
             DrawBorder();
             DrawInfo();
             while (true)
             {
+                Frame++;
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
-                    if (key.Key == ConsoleKey.Escape)
-                        return;
+
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            return;
+                        case ConsoleKey.W:
+                        case ConsoleKey.UpArrow:
+                        case ConsoleKey.Spacebar:
+                            // TODO: RotateTetromino()
+                            break;
+                        case ConsoleKey.S:
+                        case ConsoleKey.DownArrow:
+                            // TODO: MoveTetrominoDown()
+                            TetrominoRow++; // Out of bounds check
+                            Frame = 1;
+                            Score++;
+                            break;
+                        case ConsoleKey.A:
+                        case ConsoleKey.LeftArrow:
+                            // TODO: MoveTetrominoLeft()
+                            TetrominoCol--; // Out of bounds check
+                            break;
+                        case ConsoleKey.D:
+                        case ConsoleKey.RightArrow:
+                            // TODO: MoveTetrominoRight()
+                            TetrominoCol++; // Out of bounds check
+                            break;
+                    }
                 }
 
-                Score++;
+                if (Frame % MoveFrame == 0)
+                {
+                    TetrominoRow++;
+                    Frame = 0;
+                    Score++;
+                }
+
+                // TODO:
+                // if collision
+                // add piece to field array
+                // check for lines 
+                //  - increment score 
+
                 DrawBorder();
                 DrawInfo();
-                Thread.Sleep(35);
+                // TODO: DrawField();
+                DrawTetromino();
+                Thread.Sleep(50);
             }
         }
 
-        static void SetConsoleSettings(string name,
-                                       int consoleWidth,
-                                       int consoleHeight,
-                                       bool isCursorVisible = false,
-                                       ConsoleColor color = ConsoleColor.Green)
+        private static void DrawTetromino()
+        {
+            bool[,] tetromino = Tetrominos[CurrentTetromino];
+
+            for (int row = 0; row < tetromino.GetLength(0); row++)
+            {
+                for (int col = 0; col < tetromino.GetLength(1); col++)
+                {
+                    if (tetromino[row, col])
+                    {
+                        Write("*", col + 1 + TetrominoCol, row + 1 + TetrominoRow, ConsoleColor.Yellow);
+                    }
+                }
+            }
+        }
+
+        static void SetConsoleSettings(string name, int consoleWidth,
+                                       int consoleHeight, bool isCursorVisible = false)       
         {
             Console.Title = name;
             Console.CursorVisible = isCursorVisible;
             Console.SetWindowSize(consoleWidth, consoleHeight + 1);
             Console.SetBufferSize(consoleWidth, consoleHeight + 1);
-            Console.ForegroundColor = color;
         }
         
         static void DrawBorder()
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.SetCursorPosition(0, 0);
 
             // Top Line
             StringBuilder topLine = new StringBuilder();
             topLine.Append('╔');
-            topLine.Append('═', GameFieldWidth);
+            topLine.Append('═', GameFieldCol);
             topLine.Append('╦');
-            topLine.Append('═', InfoFieldWidth);
+            topLine.Append('═', InfoFieldCol);
             topLine.Append('╗');
             Console.WriteLine(topLine);
 
             // Middle Lines
-            for (int i = 0; i < GameFieldHeight; i++)
+            for (int i = 0; i < GameFieldRow; i++)
             {
                 StringBuilder midLine = new StringBuilder();
                 midLine.Append('║');
-                midLine.Append(new string(' ', GameFieldWidth));
+                midLine.Append(new string(' ', GameFieldCol));
                 midLine.Append('║');
-                midLine.Append(new string(' ', InfoFieldWidth));
+                midLine.Append(new string(' ', InfoFieldCol));
                 midLine.Append('║');
                 Console.WriteLine(midLine);
             }
@@ -78,23 +176,29 @@ namespace TetrisCsConsole
             // Bottom Line
             StringBuilder bottomLine = new StringBuilder();
             bottomLine.Append('╚');
-            bottomLine.Append('═', GameFieldWidth);
+            bottomLine.Append('═', GameFieldCol);
             bottomLine.Append('╩');
-            bottomLine.Append('═', InfoFieldWidth);
+            bottomLine.Append('═', InfoFieldCol);
             bottomLine.Append('╝');
             Console.WriteLine(bottomLine);
+
+            Console.ResetColor();
         }
 
         static void DrawInfo()
         {
-            Write("Score:", GameFieldWidth + 3, 1);
-            Write(Score.ToString(), GameFieldWidth + 3, 2);
+            Write("Score:", GameFieldCol + 3, 1, ConsoleColor.Red);
+            Write(Score.ToString(), GameFieldCol + 3, 2, ConsoleColor.Red);
+            Write("Frame:", GameFieldCol + 3, 3, ConsoleColor.Red);
+            Write(Frame.ToString(), GameFieldCol + 3, 4, ConsoleColor.Red);
         }
 
-        static void Write(string text, int col, int row)
+        static void Write(string text, int col, int row, ConsoleColor color)
         {
+            Console.ForegroundColor = color;
             Console.SetCursorPosition(col, row);
             Console.Write(text);
+            Console.ResetColor();
         }
     }
 }
